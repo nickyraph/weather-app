03ff4310763e0862bf1cbd6a3b7474863d2af044
Raphael Peter Olomi <contact@nickyraph.me>
2022-11-01 17:32:51 +0300

    GH

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
-#!/usr/bin/env sh
-# abort on errors
 set -e
-# build
+
 npm run build
-# navigate into the build output directory
+
 cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+
 git init
+
 git add -A
+
 git commit -m 'deploy'
-git push -f git@github.com:nickyraph/weather-app.git main:gh-pages
+
+git push -f git@github.com:nickyraph/weather-app.git master:gh-pages
+
 cd -
