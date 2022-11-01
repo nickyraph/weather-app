562f564d431e87b5f41ff9f932945c0be86d006d
Raphael Peter Olomi <contact@nickyraph.me>
2022-11-01 11:15:04 +0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import { Navigation } from './components/Navigation.vue'
+</script>
+
+<template>
+  
+  
+
+</template>
+
---
+++ src/components/Navigation.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex">
+        <div class="text-5xl text-white">
+            Dark Weather
+        </div>
+    </div>
+</template>
